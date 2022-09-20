958e634577fc91f3efacb19250d1728630963d2d
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-20 13:39:28 -0300

    Criando o Contexto de usuário

--- src/App.js
+++ src/App.js
@@ -10,19 +10,24 @@ import Home from './components/pages/Home';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 
+/* Contexto */
+import {UserProvider} from './context/UserContext'
+
 
 function App() {
   return (
     <Router>
-      <Navbar />
-        <Container>
-          <Routes>
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/' element={<Home />} />
-          </Routes>
-        </Container> 
-       <Footer /> 
+      <UserProvider>
+        <Navbar />
+          <Container>
+            <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/' element={<Home />} />
+            </Routes>
+          </Container> 
+        <Footer />
+       </UserProvider> 
     </Router>
   );
 }
