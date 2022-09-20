2bd7ec916809f8d8045599d47700b9dc95adbb7d
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-20 03:10:21 -0300

    Criando o componente de imput e aplicando a página de registro

---
+++ src/components/form/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Input.css";
+
+const Input = ({type, text, name, placeholder, handleOnChange, value, multiple}) => {
+  return (
+    <div>
+        <label htmlFor={name}>{text}:</label>
+        <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChange} value={value} {...(multiple ? {multiple} : '')} />
+    </div>
+  )
+}
+
+export default Input
--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Input from '../../form/Input'
 
 const Register = () => {
+
+  function handleOnChange(e) {
+
+  }
+
   return (
     <section>
-        <h1>Página de Registro</h1>
+        <h1>Registrar</h1>
+
+        <form>
+            <Input text="Nome" type="text" name="name" placeholder="Digite o seu nome" handleOnChange={handleOnChange} />
+            <Input text="Telefone" type="text" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleOnChange} />
+            <Input text="Email" type="email" name="email" placeholder="Digite o seu email" handleOnChange={handleOnChange} />
+            <Input text="Senha" type="password" name="password" placeholder="Digite a sua senha" handleOnChange={handleOnChange} />
+            <Input text="Confirmação de Senha" type="password" name="confirmpassword" placeholder="Confirme a sua senha" handleOnChange={handleOnChange} />
+            <input type="submit" value="Cadastrar" />
+        </form>
+
     </section>
   )
 }
