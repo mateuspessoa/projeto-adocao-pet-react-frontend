94060fb826eff329a5e307233994f484817fe0ac
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-20 22:18:55 -0300

    Finalizando o componente de flash message

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -1,12 +1,35 @@
 import styles from "./Message.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import bus from "../../utils/bus";
 
 const Message = () => {
 
+    const [visibility, setVisibility] = useState(false)
+    const [message, setMessage] = useState("")
     const [type, setType] = useState("")
 
+    //A tela só irá re-renderizar quando o evento acontecer
+    useEffect(() => {
+
+        bus.addListener('flash', ({message, type}) => {
+
+            setVisibility(true)
+            setMessage(message)
+            setType(type)
+
+            //Serve para a mensagem desaparecer após alguns segundos
+            setTimeout(() => {
+                setVisibility(false)
+            }, 3000)
+
+        })
+
+    }, [])
+
   return (
-    <div className={`${styles.message} ${styles[type]}`}>Minha mensagem</div>
+    visibility && (
+        <div className={`${styles.message} ${styles[type]}`}>{message}</div>
+    )
   )
 }
 
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,13 +1,21 @@
 import api from "../utils/api";
 import { useState, useEffect } from "react";
 import { useHistory } from 'react-router-dom'
+import useFlashMessage from './useFlashMessage'
 
 
 
 export default function useAuth() {
 
+    //Destruturando o setFlashMessage que vem do Hook para que o evento seja disparado
+    const { setFlashMessage } = useFlashMessage()
+
     //Hooke para registro de usuário
     async function register(user) {
+
+        //Mensagens que serão exibidas
+        let msgText = 'Cadastro realizado com sucesso!'
+        let msgType = 'success' 
 
         try {
 
@@ -18,8 +26,13 @@ export default function useAuth() {
             console.log(data)
 
         } catch(error) {
-            console.log(error)
+            //Tratamento de erros
+
+            msgText = error.response.data.message
+            msgType = 'error'
         }
+
+        setFlashMessage(msgText, msgType)
     }
 
     return { register }
