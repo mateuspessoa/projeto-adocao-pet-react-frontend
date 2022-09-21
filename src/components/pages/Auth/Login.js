108b583ec66cd4ce68e3d33f5e6ddf277d0fb020
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-20 23:11:51 -0300

    Criando o formulário de Login

--- src/components/pages/Auth/Login.js
+++ src/components/pages/Auth/Login.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useState, useContext } from 'react'
+import Input from '../../form/Input'
+import '../../form/Form.css'
+
+import { Context } from '../../../context/UserContext'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  function handleOnChange () {
+
+  }
+
   return (
-    <section>
-        <h1>Página de Login</h1>
+    <section className='form_container'>
+        <h1>Login</h1>
+        <form>
+          <Input text="Email" type="email" name="email" placeholder="Digite o seu email" handleOnChange={handleOnChange}/>
+          <Input text="Senha" type="password" name="password" placeholder="Digite a sua senha" handleOnChange={handleOnChange}/>
+
+          <input type="submit" value="Entrar" />
+        </form>
+        <p>Não tem uma conta? <Link to="/register" >Cadastre-se</Link></p>
     </section>
   )
 }
