7c3a6028459e0624ebee3307fa4aa534af93fc29
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-24 18:35:37 -0300

    Finalizando o formulário de adicionar pet (imagens)

--- src/components/form/PetForm.js
+++ src/components/form/PetForm.js
@@ -12,26 +12,43 @@ const PetForm = ({handleSubmit, petData, btnText}) => {
     const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"]
 
     function onFileChange(e) {
-
+      setPreview(Array.from(e.target.files))
+      setPet({ ...pet, images: [...e.target.files] })
     }
 
     function handleOnChange(e) {
-
+      setPet({ ...pet, [e.target.name]: e.target.value })
     }
 
     function handleColor(e) {
+      setPet({ ...pet, color: e.target.options[e.target.selectedIndex].text })
+    }
 
+    function submit(e) {
+      e.preventDefault()
+      console.log(pet)
+      //handleSubmit(pet)
     }
 
   return (
-    <form className="form_container">
+    <form onSubmit={submit} className="form_container">
+        <div className="preview_pet_images">
+          {preview.length > 0
+            ? preview.map((image, index) => 
+                <img src={URL.createObjectURL(image)} alt={pet.name} key={`${pet.name} + ${index}`} />
+            ) :
+            pet.images && pet.images.map((image, index) => 
+              <img src={`${process.env.REACT_APP_API}/images/pets/${image}`} alt={pet.name} key={`${pet.name} + ${index}`} />
+            )}
+        </div>
+
         <Input text="Imagens do Pet" type="file" name="images" handleOnChange={onFileChange} multiple={true} />
 
         <Input text="Nome do Pet" type="text" name="name" placeholder="Digite o nome" handleOnChange={handleOnChange} value={pet.name || ''}/>
 
-        <Input text="Idade do Pet" type="text" name="age" placeholder="Digite a idade" handleOnChange={handleOnChange} value={pet.age || ''}/>
+        <Input text="Idade do Pet" type="number" name="age" placeholder="Digite a idade" handleOnChange={handleOnChange} value={pet.age || ''}/>
 
-        <Input text="Peso do Pet" type="number" name="weight" placeholder="Digite o peso" handleOnChange={handleOnChange} value={pet.name || ''}/>
+        <Input text="Peso do Pet" type="number" name="weight" placeholder="Digite o peso" handleOnChange={handleOnChange} value={pet.weight || ''}/>
 
         <Select name="color" text="Selecione a cor" options={colors} handleOnChange={handleColor} value={pet.color || ''} />
 
