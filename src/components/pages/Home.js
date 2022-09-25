23223622ee280c937b50af68e8df50897d818094
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-25 00:00:37 -0300

    CSS da Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -20,23 +20,25 @@ const Home = () => {
 
   return (
     <section>
-        <div>
+        <div className="pet_home_header">
           <h1>Adote um Pet</h1>
           <p>Veja os detalhes dos Pets e entre em contato com o tutor</p>
         </div>
 
-        <div>
+        <div className="pet_container">
           {pets.length > 0 && 
             pets.map((pet) => (
-              <div>
-                <p>Imagem do Pet</p>
+              <div className="pet_card">
+
+                <div style={{backgroundImage: `url(${process.env.REACT_APP_API}/images/pets/${pet.images[0]})`}} className="pet_card_image"></div>
+
                 <h3>{pet.name}</h3>
                 <p><span className="bold">Peso: </span>{pet.weight}Kg</p>
 
                 {pet.available ? (
                   <Link to={`pet/${pet._id}`}>Mais Detalhes</Link>
                 ) : (
-                  <p>Adotado</p>
+                  <p className="adopter_text">Adotado</p>
                 )}
               </div>
             ))
