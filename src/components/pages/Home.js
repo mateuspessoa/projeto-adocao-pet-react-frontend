a71e2d1cf061137170cb63e767fae332f15f1a23
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-24 23:42:53 -0300

    Exibindo todos os pets na Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -20,7 +20,31 @@ const Home = () => {
 
   return (
     <section>
-        <h1>Página Home</h1>
+        <div>
+          <h1>Adote um Pet</h1>
+          <p>Veja os detalhes dos Pets e entre em contato com o tutor</p>
+        </div>
+
+        <div>
+          {pets.length > 0 && 
+            pets.map((pet) => (
+              <div>
+                <p>Imagem do Pet</p>
+                <h3>{pet.name}</h3>
+                <p><span className="bold">Peso: </span>{pet.weight}Kg</p>
+
+                {pet.available ? (
+                  <Link to={`pet/${pet._id}`}>Mais Detalhes</Link>
+                ) : (
+                  <p>Adotado</p>
+                )}
+              </div>
+            ))
+          }
+          {pets.length === 0 && (
+            <p>Não há pets disponíveis para adoção no momento</p>
+          )}
+        </div>
     </section>
   )
 }
