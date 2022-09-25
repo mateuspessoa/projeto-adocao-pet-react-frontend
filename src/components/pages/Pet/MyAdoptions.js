050f5f9d91144aa703026e40d2ce56187841b0cb
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-25 10:46:02 -0300

    Finalizando a página de adoções

--- src/components/pages/Pet/MyAdoptions.js
+++ src/components/pages/Pet/MyAdoptions.js
@@ -21,9 +21,43 @@ const MyAdoptions = () => {
     }, [token])
 
   return (
-    <div>
-        <h1>Minhas Adoções</h1>
-    </div>
+    <section>
+        <div className='petlist_header'>
+            <h1>Minhas Adoções</h1>
+        </div>
+
+        <div className="petlist_container">
+            {pets.length > 0 && 
+                pets.map((pet) => (
+                    <div className='petlist_row' key={pet._id}>
+                        <RoundedImage src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`} alt={pet.name} width="px75" />
+                        <span className="bold">{pet.name}</span>
+
+                        <div className='contacts'>
+                            <p>
+                                <span className="bold">Ligue para: </span> {pet.user.phone}
+                            </p>
+
+                            <p>
+                                <span className="bold">Fale com: </span> {pet.user.name}
+                            </p>
+                        </div>
+
+                        <div className="actions">
+                            {pet.available ? (
+                            <>
+                                <p>Adoção em processo</p>
+                            </>
+                            ) : ( 
+                        
+                        <p>Parabéns por concluir a adoção</p>
+                        )}
+                        </div>
+                    </div>
+                ))}
+            {pets.length === 0 && <p>Você ainda não tem adoções de pets.</p>}
+        </div>
+    </section>
   )
 }
 
