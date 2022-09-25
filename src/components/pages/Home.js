597e130d3c08252d5b0787184aac0945a14d3cce
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-24 23:27:09 -0300

    Resgatando todos os pets na Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import api from "../../utils/api"
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+import './Home.css'
 
 const Home = () => {
+
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+
+    api.get('/pets').then((response) => {
+      setPets(response.data.pets)
+    })
+
+
+  }, [])
+
+
   return (
     <section>
         <h1>Página Home</h1>
