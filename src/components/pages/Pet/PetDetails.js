da6ff5d27d451d7aa1b152129882650ab9f5cf3e
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-25 00:57:26 -0300

    Estruturando a página de detalhes do pet

--- src/components/pages/Pet/PetDetails.js
+++ src/components/pages/Pet/PetDetails.js
@@ -19,9 +19,30 @@ const PetDetails = () => {
     }, [id])
 
   return (
-    <div>
-        <h1>{pet.name}</h1>
-    </div>
+    <>
+        {pet.name && (
+            <section>
+                <div>
+                    <h1>Conhecendo o Pet: {pet.name}</h1>
+                    <p>Tem interesse em adotar esse pet? Marque uma visita agora mesmo</p>
+                </div>
+
+                <div>
+                {pet.images.map((image, index) => (
+                        <img key={index} src={`${process.env.REACT_APP_API}/images/pets/${image}`} alt={pet.name}/>
+                    ))}
+                </div>
+                <p><span className="bold">Peso: </span>{pet.weight}Kg</p>
+                <p><span className="bold">Idade: </span>{pet.age} anos</p>
+
+                {token ? (
+                    <button>Solicitar Visita</button>
+                ) : (
+                    <p><Link to="/register">Crie uma conta para adotar um pet</Link></p>
+                )}
+            </section>
+        )}
+    </>
   )
 }
 
