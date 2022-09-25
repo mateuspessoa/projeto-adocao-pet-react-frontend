24eaf44332bd9575aca3d6581089cda18d9ea25d
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-25 01:31:45 -0300

    Função de agendar visita

--- src/components/pages/Pet/PetDetails.js
+++ src/components/pages/Pet/PetDetails.js
@@ -1,47 +1,80 @@
-import './PetDetails.css'
 import api from '../../../utils/api'
+
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 
+import styles from './PetDetails.css'
+
+/* hooks */
 import useFlashMessage from '../../../hooks/useFlashMessage'
 
-const PetDetails = () => {
+function PetDetails() {
+  const [pet, setPet] = useState({})
+  const { id } = useParams()
+  const { setFlashMessage } = useFlashMessage()
+  const [token] = useState(localStorage.getItem('token') || '')
 
-    const [pet, setPet] = useState({})
-    const {id} = useParams()
-    const {setFlashMessage} = useFlashMessage()
-    const [token] = useState(localStorage.getItem('token') || '')
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet)
+    })
+  }, [id])
 
-    useEffect(() => {
-        api.get(`/pets/${id}`).then((response) => {
-            setPet(response.data.pet)
-        })
-    }, [id])
+  async function schedule() {
+    let msgType = 'success'
+
+    const data = await api
+      .patch(`pets/schedule/${pet._id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+  }
 
   return (
     <>
-        {pet.name && (
-            <section className='pet_details_container'>
-                <div className='pet_details_header'>
-                    <h1>Conhecendo o Pet: {pet.name}</h1>
-                    <p>Tem interesse em adotar esse pet? Marque uma visita agora mesmo</p>
-                </div>
-
-                <div className='pet_images'>
-                {pet.images.map((image, index) => (
-                        <img key={index} src={`${process.env.REACT_APP_API}/images/pets/${image}`} alt={pet.name}/>
-                    ))}
-                </div>
-                <p><span className="bold">Peso: </span>{pet.weight}Kg</p>
-                <p><span className="bold">Idade: </span>{pet.age} anos</p>
-
-                {token ? (
-                    <button>Solicitar Visita</button>
-                ) : (
-                    <p><Link to="/register">Crie uma conta para adotar um pet</Link></p>
-                )}
-            </section>
-        )}
+      {pet.name && (
+        <section className="pet_details_container">
+          <div className="petdetails_header">
+            <h1>Conhecendo o Pet: {pet.name}</h1>
+            <p>Se tiver interesse, marque uma visita para conhecê-lo!</p>
+          </div>
+          <div className="pet_images">
+            {pet.images.map((image, index) => (
+              <img
+                key={index}
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                alt={pet.name}
+              />
+            ))}
+          </div>
+          <p>
+            <span className="bold">Peso:</span> {pet.weight}kg
+          </p>
+          <p>
+            <span className="bold">Idade:</span> {pet.age} anos
+          </p>
+          {token ? (
+            <button onClick={schedule}>Solicitar uma Visita</button>
+          ) : (
+            <p>
+              Você precisa <Link to="/register">criar uma conta</Link> para
+              solicitar a visita.
+            </p>
+          )}
+        </section>
+      )}
     </>
   )
 }
